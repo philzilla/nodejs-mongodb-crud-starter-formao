8495f5721efcb25a9f38e44f789f62a23db6233a
philzilla <dev@philippeyim.fr>
2021-01-07 23:16:14 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+
+const app = express();
+
+// Handlebars
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs', handlebars: allowInsecurePrototypeAccess(Handlebars)}));
+app.set('view engine', 'hbs')
+
+// BodyParser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+// MongoDB
+
+
+// Routes
+
+
+
+app.listen(4000, function() {
+    console.log("écoute le port 4000");
+    
+})
